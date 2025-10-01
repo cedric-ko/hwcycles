611fd774a7060055efe4312af3d230df0c398e39
Alexey Utkin <cedrickobayashi@gmail.com>
2025-10-01 23:28:26 +0300

    Initial Git

--- src/test/java/ru/netology/services/VacationServiceTest.java
+++ src/test/java/ru/netology/services/VacationServiceTest.java
@@ -2,27 +2,34 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class VacationServiceTest {
 
-    @Test
-    public void testHighIncome() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "3,10000,3000,20000",
+//            "2,100000,60000,150000"
+//    })
+    @CsvFileSource(files = "src/test/resources/vacation.csv")
+    public void testLowValues(int expected, int income, int expenses, int threshold) {
         VacationService service = new VacationService();
 
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
+        // int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void testLowIncome() {
-        VacationService service = new VacationService();
-
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
+//    @Test
+//    public void testHighValues() {
+//        VacationService service = new VacationService();
+//
+//        int expected = 2;
+//        int actual = service.calculate(100000, 60000, 150000);
+//
+//        Assertions.assertEquals(expected, actual);
+//    }
 }
-
